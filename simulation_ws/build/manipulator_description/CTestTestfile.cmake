81e1c7b8c109e0ba2e6a207bf7bb2466a249216f
Javier Macías <jamaciastf@gmail.com>
2019-07-07 19:28:59 +0200

    Added simulation workspace with basic URDF with visualization in rviz

---
+++ simulation_ws/build/manipulator_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/ROS/ROSManipulator/simulation_ws/src/manipulator_description
+# Build directory: /mnt/d/ROS/ROSManipulator/simulation_ws/build/manipulator_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
