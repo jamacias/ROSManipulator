62e8dce2f1c651a74186578346a1ff4ecb9cf388
Javier Macías <jamaciastf@gmail.com>
2019-07-07 22:52:11 +0200

    .gitignore fix

--- simulation_ws/build/manipulator_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/ROS/ROSManipulator/simulation_ws/src/manipulator_description
-# Build directory: /mnt/d/ROS/ROSManipulator/simulation_ws/build/manipulator_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
